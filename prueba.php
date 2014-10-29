2b5db493101600ed9da0d901572a7b30d209b64e
CarlosJBarahona <ingbarahonacarlos@hotmail.com>
2014-10-29 13:13:56 -0600

    primera carga

---
+++ prueba.php
@@ -0,0 +1,26 @@
+
+    <link rel="stylesheet" type="text/css" href="css/bordes.css">
+    <style type="text/css">
+        body{background-color: white}
+    </style>
+<?php 
+$i="LOREM";
+ ?>
+<h3 style="font-family: OpenSans-Bold"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-ExtraBold"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-Regular"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-BoldItalic"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-ExtraBoldItalic"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-Italic"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-LightItalic"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-Light"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-Semibold"><?php echo $i ?></h3> 
+<h3 style="font-family: OpenSans-SemiboldItalic"><?php echo $i ?></h3> 
+
+<!--
+<div class="fondoimg imgcont">
+      <div class="imgrosa">
+          <h2 class="allpro">BORDADOS</h2>
+          <h5>LOREM IMPSU AUTOR ALIQUET AENEAN SOLLISITUDIN,</h5>
+      </div>
+</div>-->
